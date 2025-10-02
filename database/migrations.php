9deeda8031f0522aa6f77982557834ff81d7ee4a
ALPHAWOLF\munda <benjamin.mundama@strathmore.edu>
2025-10-02 15:21:39 +0300

    created migrations v2

--- database/migrations.php
+++ database/migrations.php
@@ -11,8 +11,12 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+// Drop table if exists
+$sql = "DROP TABLE IF EXISTS MyGuests";
+$conn->query($sql);
+
 // sql to create table
-$sql = "CREATE TABLE MyGuests (
+$sql = "CREATE TABLE IF NOT EXISTS MyGuests (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 firstname VARCHAR(30) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
@@ -22,6 +26,24 @@ reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 
 if ($conn->query($sql) === TRUE) {
   echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+// Drop table if exists
+$sql = "DROP TABLE IF EXISTS users";
+$conn->query($sql);
+
+// create table users
+$sql = "CREATE TABLE IF NOT EXISTS users (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(30) NOT NULL,
+password VARCHAR(255) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+if ($conn->query($sql) === TRUE) {
+  echo "Table users created successfully";
 } else {
   echo "Error creating table: " . $conn->error;
 }
